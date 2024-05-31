bedd58508aea6b3b087765ae029d73a32c80b295
Admin <Admin@DESKTOP-58EU95I>
2024-05-30 20:14:37 -0600

    Agregar archivos de proyecto.

---
+++ Complementario045834157/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Complementario045834157
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Complementario045834157/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace Complementario045834157
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            // Obtendra los valores ingredados en los diferentes entry
+            if (int.TryParse(NumberEntry.Text, out int number) && number > 0)
+            {
+                //Hará la conversión de los numeros positivos enteros ingresados(Triangulo y cubo)
+                int triangulo = number * number;
+                int cubo = number * number * number;
+
+                //Mostrará el resultado de la ecuación realizada del triangulo y cubo del numero positivo
+                TrianguloResultado.Text = $"Cuadrado: {triangulo}";
+                CuboResultado.Text = $"Cubo: {cubo}";
+            }
+            
+        }
+    }
+
+}
